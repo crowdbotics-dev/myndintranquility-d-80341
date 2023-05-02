8a6c170fb0e0152c99160090ae811d463c05cea0
Crowdbotics <engineering@crowdbotics.com>
2023-05-02 13:21:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "myndintranquility-d-80341",
+  url: "https://myndintranquility-d-80341.botics.co",
+  api: "https://myndintranquility-d-80341.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
